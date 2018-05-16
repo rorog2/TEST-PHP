67474faef97238420a94d6293bfe787e5fd29f81
rorog2 <rorog2@live.fr>
2018-05-16 19:27:04 +0200

    Ajout du code minimal dans le index.php de l'upload de fichiers

---
+++ upload_fichiers/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Page d'acceuil des TESTS</title>
+		<meta charset="utf-8" />
+		<link rel="stylesheet" href="style.css" />
+		<script src="script.js"></script>
+	</head>
+	<body>
+		<header>
+		</header>
+
+		<main>
+			<?php phpinfo(); ?>
+		</main>
+
+		<footer>
+		</footer>
+	</body>
+</html>
